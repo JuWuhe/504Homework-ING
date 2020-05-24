3a64bc34739d56e43e443f29d079a46b41835d95
JuWuhe <Insyent@foxmail.com>
2020-05-24 22:42:33 +0800

    C++第3版第七章作业

---
+++ c.njupt.edu.cn/6.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <class T>
+void swap(T &a,T &b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    double a,b;
+    std::cin >> a >> b;
+    swap(a,b);
+    std::cout << a << " " << b;
+    return 0;
+}
